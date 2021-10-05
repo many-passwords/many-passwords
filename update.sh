f0dd0506163838dc8b7e725c600b52918fde2ad2
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-10-05 18:17:24 +0530

    Updated Current Script to count only Unique Rows

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@ README="README.md"
 PASSWORDS="passwords.csv"
 
 echo "Counting passwords..."
-WORD_COUNT=$(($(cat $PASSWORDS | wc -l) - 1))
 
 echo "Sorting passwords..."
 cp ${PASSWORDS} ${PASSWORDS}2
-cat ${PASSWORDS}2 | (sed -u 1q; sort -f) > $PASSWORDS
+cat ${PASSWORDS}2 | (sed -u 1q; sort -u -f) > $PASSWORDS
+WORD_COUNT=$(($(cat $PASSWORDS | wc -l) - 1))
 rm ${PASSWORDS}2
 
 echo "Updating README.md"
