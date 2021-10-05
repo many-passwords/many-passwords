fc154c9cd2b676d8b9aec6c1f041ef06254f2095
Michał Piechowiak <michalp2404@gmail.com>
2021-10-05 21:54:31 +0200

    Better description

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 README="README.md"
 PASSWORDS="passwords.csv"
 
-echo "Counting passwords..."
-
-echo "Sorting passwords..."
+echo "Sorting and remove duplicates of passwords..."
 cp ${PASSWORDS} ${PASSWORDS}2
 cat ${PASSWORDS}2 | (sed -u 1q; sort -u -f) > $PASSWORDS
-WORD_COUNT=$(($(cat $PASSWORDS | wc -l) - 1))
 rm ${PASSWORDS}2
+
+echo "Counting passwords..."
+WORD_COUNT=$(($(cat $PASSWORDS | wc -l) - 1))
 
 echo "Updating README.md"
 sed -i -E "s/(Number of collected default passwords: \`\`).*(\`\`\. )/\1$(echo $WORD_COUNT)\2/" README.md
